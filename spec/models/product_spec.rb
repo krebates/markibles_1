00186ed96c03470f2a68cd1081f085fd4c9e2850
Krystle Bates <krebates@gmail.com>
2013-12-29 13:08:52 -0500

    updated product and rating tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,5 +4,7 @@ describe Product do
   it {should validate_presence_of :name}
   it {should validate_presence_of :description}
   it {should validate_presence_of :price}
-  # it {should **be a price**}
+  it {should have_many :ratings}
+  it {should belong :seller}
+  it {should validate_numericality_of :price}
 end
--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -1,10 +1,13 @@
 require 'spec_helper'
 
 describe Rating do
-it {should validate_presence_of :overall_rating}
-it {should validate_presence_of :flavor}
-it {should validate_presence_of :presentation}
-it {should validate_presence_of :timeliness_of_delivery}
+it { should validate_presence_of :overall_rating }
+it { should validate_presence_of :flavor }
+it { should validate_presence_of :presentation }
+it { should validate_presence_of :timeliness_of_delivery }
+it { should belong_to(:user) }
+it { should belong_to(:seller).dependent(:destroy) }
+it { should belong_to(:product).dependent(:destroy)}
 
 # destroy if product deleted
 # destroy if seller deleted
