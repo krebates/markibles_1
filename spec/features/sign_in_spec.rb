04a0dc9629f7d120a276e8409c7949ca858976d2
Krystle Bates <krebates@gmail.com>
2014-01-04 21:28:58 -0500

    completed user sign up process

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -17,28 +17,44 @@ feature 'signs up', %Q{
     visit root_path
 
     click_link 'Sign Up'
+    # within("form#new_user") do
+      fill_in 'Username', with: 'catdog'
+      fill_in 'First Name', with: 'Krystle'
+      fill_in 'Last Name', with: 'Bates'
+      fill_in 'Email', with: 'user@example.com'
+      fill_in 'user_password', with: 'words12345'
+      fill_in 'Password Confirmation', with: 'words12345'
 
-    fill_in 'Username', with: 'catdog'
-    fill_in 'First Name', with: 'Krystle'
-    fill_in 'Last Name', with: 'Bates'
-    fill_in 'Email', with: 'user@example.com'
-    save_and_open_page
-
-    within("#user_password") do
-      fill_in 'Password', with: 'password'
-    end
-
-    fill_in 'Password Confirmation', with: 'password'
-
-    click_button 'Sign Up'
+      click_button 'Sign Up'
+    # end
 
     expect(page).to have_content("You're In!")
 
     expect(page).to have_content("Sign Out")
   end
 
-  scenario 'required information is not supplied'
-    # visit
+  scenario 'required information is not supplied' do
+    visit root_path
+    click_link 'Sign Up'
+    click_button 'Sign Up'
 
-  scenario 'password confirmation does not match confirmation'
+    expect(page).to have_content("can't be blank")
+    expect(page).to_not have_content("Sign Out")
+  end
+
+
+  scenario 'password confirmation does not match confirmation' do
+    visit root_path
+    click_link 'Sign Up'
+
+    fill_in 'user_password', with: 'password'
+    fill_in 'Password Confirmation', with: 'differentpassword'
+
+    click_button 'Sign Up'
+
+    expect(page).to have_content("doesn't match")
+    expect(page).to_not have_content("Sign Out")
+  end
+
+
 end
