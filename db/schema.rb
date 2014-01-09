92298455f1e8f89609c7882e3a60ccd2a8e579cb
Krystle Bates <krebates@gmail.com>
2014-01-09 14:24:36 -0500

    updated schema for category id in item

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140109010908) do
+ActiveRecord::Schema.define(version: 20140109191456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 20140109010908) do
     t.decimal  "price",       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "category_id"
+    t.integer  "category_id", null: false
   end
 
   create_table "ratings", force: true do |t|
--- spec/features/sellers/seller_creates_store_spec.rb
+++ spec/features/sellers/seller_creates_store_spec.rb
@@ -1,19 +1,30 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# #  As a buyer,
-# #  I want to be able to create an account,
-# #  so that I can buy items on the account
+#  As a buyer,
+#  I want to be able to create an account,
+#  so that I can buy items on the account
 
-# # I should see an area where I can create a new account.
-# # I must include my name, email, and description of myself.
-# # I must include a photo for my profile.
+# I should see an area where I can create a new account.
+# I must include my name, email, and description of myself.
+# I must include a photo for my profile.
 
-# feature 'user creates a store to sell items'
+feature 'user creates a store to sell items'
 
-#   scenario 'user completes form will all valid fields'
-#     visit
+  scenario 'user completes form will all valid fields'
+    category = FactoryGirl.create(:category)
 
-#   scenario 'user does not complete form with all valid fields'
+    visit root
+    click_on "List a Food Item!"
+    fill_in "Name", with: "Bourbon Cake"
+    fill_in "Photo", with: "Photo"
+    fill_in "Description", with: "Sweet sweet goodness"
+    fill_in "Price", with: 5.00
+    select  "Cake", from: "Category"
+    click_on "Add item"
 
-# end
+    expect(page).to have_content "Bourbon Cake"
+    expect(page).to have_content "Sweet sweet goodness"
+
+  scenario 'user does not complete form with all valid fields'
+end
 
